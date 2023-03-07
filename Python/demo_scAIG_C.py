e3ccf9951b98d1985daa656afb9186009cb5a72e
BioCom-Lab <125749484+BioCom-Lab@users.noreply.github.com>
2023-03-07 22:30:54 +0800

    Add files via upload

---
+++ Python/demo_scAIG_C.py
@@ -0,0 +1,25 @@
+import numpy as np
+import time
+from scAIG_C import scAIG_C
+from sklearn.metrics.cluster import adjusted_rand_score as ari
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+Datasets = ['Goolam']
+n_datasets = len(Datasets)
+for id in range(1):
+    data = Datasets[id]
+    print(data)
+    fea = np.load(data+'_cell.npy')
+    gnd = np.load(data+'_label.npy')
+    for rep in range(1):
+        t1 = time.time()
+        [num_cluster,idx_cell,grp,Q,C,A,idx_gene] = scAIG_C(fea)
+        t2 = time.time()
+        labels = gnd-np.min(gnd)+1
+        labels = labels[idx_cell]
+        labels = labels-1
+        labels = labels.reshape(grp.shape)
+        ARI = ari(labels,grp)
+        print('ARI=%2,4f.\n',ARI)
+        print('Clustering CPU Time = %2,4f.\n',t2-t1)
